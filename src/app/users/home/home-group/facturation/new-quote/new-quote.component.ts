47a90bb6549216d0d8b21b016cc1d6e8954ab0c8
RyanFoerster <ryanfoerster@outlook.be>
2024-07-21 17:41:55 +0200

    Formulaire nouveau client

--- src/app/users/home/home-group/facturation/new-quote/new-quote.component.ts
+++ src/app/users/home/home-group/facturation/new-quote/new-quote.component.ts
@@ -15,18 +15,59 @@ import {HlmSelectImports} from '@spartan-ng/ui-select-helm';
 import {HlmButtonDirective} from "@spartan-ng/ui-button-helm";
 import {HlmIconComponent} from "@spartan-ng/ui-icon-helm";
 import {provideIcons} from "@ng-icons/core";
-import {lucideEdit, lucideFileDown, lucidePlus, lucidePlusCircle, lucideTrash} from "@ng-icons/lucide";
+import {
+  lucideCheck,
+  lucideEdit,
+  lucideFileDown,
+  lucidePlus,
+  lucidePlusCircle,
+  lucideTrash,
+  lucideXCircle
+} from "@ng-icons/lucide";
 import {EuroFormatPipe} from "../../../../../shared/pipes/euro-format.pipe";
+import {BrnDialogContentDirective, BrnDialogTriggerDirective} from '@spartan-ng/ui-dialog-brain';
+import {
+  HlmDialogComponent,
+  HlmDialogContentComponent,
+  HlmDialogDescriptionDirective,
+  HlmDialogFooterComponent,
+  HlmDialogHeaderComponent,
+  HlmDialogTitleDirective,
+} from '@spartan-ng/ui-dialog-helm';
 
 @Component({
   selector: 'app-new-quote',
   standalone: true,
-  imports: [HlmInputDirective, HlmLabelDirective, HlmSeparatorDirective, BrnSeparatorComponent, BrnSelectImports, HlmSelectImports, HlmButtonDirective, HlmIconComponent, HlmCaptionComponent,
+  imports: [
+    HlmInputDirective,
+    HlmLabelDirective,
+    HlmSeparatorDirective,
+    BrnSeparatorComponent,
+    BrnSelectImports,
+    HlmSelectImports,
+    HlmButtonDirective,
+    HlmIconComponent,
+    HlmCaptionComponent,
     HlmTableComponent,
     HlmTdComponent,
     HlmThComponent,
-    HlmTrowComponent, EuroFormatPipe,],
-  providers: [provideIcons({lucidePlusCircle, lucideTrash, lucideEdit, lucideFileDown, lucidePlus})],
+    HlmTrowComponent,
+    EuroFormatPipe,
+    BrnDialogTriggerDirective,
+    BrnDialogContentDirective,
+
+    HlmDialogComponent,
+    HlmDialogContentComponent,
+    HlmDialogHeaderComponent,
+    HlmDialogFooterComponent,
+    HlmDialogTitleDirective,
+    HlmDialogDescriptionDirective,
+
+    HlmLabelDirective,
+    HlmInputDirective,
+    HlmButtonDirective,
+  ],
+  providers: [provideIcons({lucidePlusCircle, lucideTrash, lucideEdit, lucideFileDown, lucidePlus, lucideCheck, lucideXCircle})],
   templateUrl: './new-quote.component.html',
   styleUrl: './new-quote.component.css'
 })

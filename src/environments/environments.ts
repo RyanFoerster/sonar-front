dd614f8e9af9461bde6c3890e4ab6d0b7a60976a
RyanFoerster <ryanfoerster@outlook.be>
2024-08-08 14:16:38 +0200

    Heroku

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environments = {
     production: false,
-    API_URL: "https://infplqscifnnjfbatsfh.supabase.co",
+    API_URL: "https://sonarartists-81801f88396b.herokuapp.com",
     TOKEN_KEY: "token",
     REFRESH_TOKEN_KEY: "refresh_token",
     USER_KEY: "user"

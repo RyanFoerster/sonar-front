0275efbd2fec9094f2faee9437948fde95455943
Manon <manonaussems@dimagin.be>
2024-08-09 11:13:39 +0200

    Manon privacy policy, usage policy, modif diary

---
+++ src/app/usage-policy/usage-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usage-policy',
+  standalone: true,
+  imports: [],
+  templateUrl: './usage-policy.component.html',
+  styleUrl: './usage-policy.component.css'
+})
+export class UsagePolicyComponent {
+
+}

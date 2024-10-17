a864cbca45cc75ecd89d98918a99649ed5f4bfe9
RyanFoerster <ryanfoerster@outlook.be>
2024-10-17 08:21:35 +0200

    Change environment api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'sonar-back.vercel.app',
+  API_URL: 'web-production-8114.up.railway.app',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

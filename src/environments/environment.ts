eb4c6290447623f856dd449b56d616c567b36c99
RyanFoerster <ryanfoerster@outlook.be>
2024-10-29 13:50:04 +0100

    api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://82.112.255.83:3000',
+  API_URL: 'http://82.112.255.83:8090',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

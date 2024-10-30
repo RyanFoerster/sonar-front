5c2c2ec4f66a5388cad0f9da9eb6a66b500b0772
delhombre <brunodogbase@gmail.com>
2024-10-30 13:13:17 +0100

    📝 (environment.ts): update API_URL to use HTTPS instead of HTTP for improved security and change the endpoint path to /backend for better organization

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://sonarartists.fr:8090',
+  API_URL: 'https://sonarartists.fr/backend',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

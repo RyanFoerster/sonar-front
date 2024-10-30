e34cfba865cf1fdcf8005d966aca8ed3a7b627f3
delhombre <brunodogbase@gmail.com>
2024-10-30 11:22:23 +0100

    ✨ (environment.ts): update API_URL to use HTTPS protocol for better security and change hostname to 'sonarartists.fr' to reflect current production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:8090',
+  API_URL: 'https://sonarartists.fr:8090/',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

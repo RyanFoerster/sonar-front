8dd7bc376b3f48972f171892008988bd6345960c
delhombre <brunodogbase@gmail.com>
2024-10-30 13:56:04 +0100

    📝 (environment.ts): Update API_URL to new server IP address to reflect the actual endpoint of the API and ensure proper communication with the server.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:8090',
+  API_URL: 'http://82.112.255.83:8090',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

0ea2d326e1fea7b876aabdf4e839940a8d083625
delhombre <brunodogbase@gmail.com>
2024-10-30 14:48:09 +0100

    🚀 (environment.ts): update API_URL to point to new domain with HTTPS for better security and privacy.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://82.112.255.83:8090',
+  API_URL: 'https://sonarartists.fr:9090',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

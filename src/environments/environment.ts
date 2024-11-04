858bcb079fc3546e2e84608d6d3def8296432c13
delhombre <brunodogbase@gmail.com>
2024-11-04 10:56:37 +0100

    📝 (environment.ts): update API_URL value from explicit port to a more descriptive endpoint path.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://sonarartists.fr:9090',
+  API_URL: 'https://sonarartists.fr/backend',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',

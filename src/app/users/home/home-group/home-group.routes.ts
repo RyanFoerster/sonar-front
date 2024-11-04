661aab67d3053add1ceca405895827d484134b7b
RyanFoerster <ryanfoerster@outlook.be>
2024-11-04 09:16:48 +0100

    Note de crédit

--- src/app/users/home/home-group/home-group.routes.ts
+++ src/app/users/home/home-group/home-group.routes.ts
@@ -8,30 +8,30 @@ export default [
       import('./home-group.component').then((m) => m.HomeGroupComponent),
     canActivate: [authGuard],
   },
-  {
-    path: ':id/facturation',
-    loadComponent: () =>
-      import('./facturation/facturation.component').then(
-        (m) => m.FacturationComponent,
-      ),
-    canActivate: [authGuard],
-  },
-  {
-    path: ':id/facturation/new-quote',
-    loadComponent: () =>
-      import('./facturation/new-quote/new-quote.component').then(
-        (m) => m.NewQuoteComponent,
-      ),
-    canActivate: [authGuard],
-  },
-  {
-    path: ':id/facturation/credit-note',
-    loadComponent: () =>
-      import('./facturation/credit-note/credit-note.component').then(
-        (m) => m.CreditNoteComponent,
-      ),
-    canActivate: [authGuard],
-  },
+  // {
+  //   path: ':id/facturation',
+  //   loadComponent: () =>
+  //     import('./facturation/facturation.component').then(
+  //       (m) => m.FacturationComponent,
+  //     ),
+  //   canActivate: [authGuard],
+  // },
+  // {
+  //   path: ':id/facturation/new-quote',
+  //   loadComponent: () =>
+  //     import('./facturation/new-quote/new-quote.component').then(
+  //       (m) => m.NewQuoteComponent,
+  //     ),
+  //   canActivate: [authGuard],
+  // },
+  // {
+  //   path: ':id/facturation/credit-note',
+  //   loadComponent: () =>
+  //     import('./facturation/credit-note/credit-note.component').then(
+  //       (m) => m.CreditNoteComponent,
+  //     ),
+  //   canActivate: [authGuard],
+  // },
   {
     path: ':id/membership',
     loadComponent: () =>

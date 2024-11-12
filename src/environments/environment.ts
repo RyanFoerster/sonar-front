3bebb06769fe6586a1ea90cab1f08b0a15fed4cf
delhombre <brunodogbase@gmail.com>
2024-11-12 12:48:17 +0100

    🔧 chore(workflows): reconfigure GitHub Actions workflows
    - delete build.yaml and docker-compose.yaml
    - split build and deploy steps into separate workflows
    - add deploy.yml for production environment
    - update docker-image.yml to include pull requests
    
    ♻️ refactor(environment): adjust API_URL for backend connectivity
    
    - change API_URL from full URL to relative path 'backend' for flexible environment configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://sonarartists.fr/backend',
+  API_URL: 'backend',
   TOKEN_KEY: 'token',
   REFRESH_TOKEN_KEY: 'refresh_token',
   USER_KEY: 'user',
